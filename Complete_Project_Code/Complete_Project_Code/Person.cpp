8ef4629702d741698d02dc4ffaba6342263ffa10
syedmaqbolagha <syedmaqbolagha000123@gmail.com>
2026-04-26 23:25:49 +0500

    Clean repo reset

--- Complete_Project_Code/Complete_Project_Code/Person.cpp
+++
@@ -1,7 +0,0 @@
-#include "Person.h"
-
-Person::Person(string n, int a) : name(n), age(a) {}
-Person::~Person() {}
-
-string Person::getName() const { return name; }
-int Person::getAge() const { return age; }
